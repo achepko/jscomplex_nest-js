5142dd950b237f22694fbe19d6a61f45f21c83fa
Anton Chepko <choptonys@gmail.com>
2023-07-29 20:49:32 +0200

    2 lesson connect to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AnimalController } from './animal/animal.controller';
 import { AnimalModule } from './animal/animal.module';
 import { UserService } from './user/user.service';
+import { PostgresqlConfigModule } from './config/database/config.module';
+import { TypeOrmConfiguration } from './config/database/type-orm-configuration';
 
 @Module({
-  imports: [UserModule, AnimalModule],
+  imports: [
+    TypeOrmModule.forRootAsync(TypeOrmConfiguration.config),
+    UserModule,
+    AnimalModule,
+    PostgresqlConfigModule,
+  ],
   controllers: [AppController, AnimalController],
   providers: [AppService, UserService],
 })
